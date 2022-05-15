086afe7e381a0c4a23a8b12286d5b916ea990435
Franco Reyes Mac <franco.javier.reyes@gmail.com>
2022-05-15 14:33:39 +1000

    Commands for changing server settings

---
+++ src/cogs/settings.py
@@ -0,0 +1,83 @@
+'''
+Cog for server settings commands
+'''
+
+import discord
+from discord.ext import commands
+
+from src import constants
+from src.data import data
+
+class SettingsCog(commands.Cog):
+    def __init__(self, bot: discord.Bot):
+        self.bot = bot
+    
+    @commands.slash_command()
+    async def settingss(self, ctx: discord.ApplicationContext):
+        """See the settings for this server"""
+        offset = data.get_offset(ctx.guild_id)
+        offset = f"{'+' if offset >= 0 else ''}{offset}"
+
+        target = data.get_offset(ctx.guild_id)
+        target = f'<#{target}>' if target else 'None'
+
+        embed = {'title': 'Reminder Bot Settings'}
+        embed['fields'] = [
+            {
+                'name': '🕒 UTC Offset',
+                'value': 'All times entered in this server will be offset from UTC Time by this many hours.\n\n'
+                        f'Current UTC Offset: {offset}\n\n'
+                        'Use `/settings offset <offset>` to set the offset.\n'
+            },
+            {
+                'name': '📢 Reminder Channel',
+                'value': 'If specified, all reminders will be sent in this channel. Otherwise, reminders are sent to the channel they were set in.\n\n'
+                        f'Current Reminder Channel: {target}\n\n'
+                        'Use `/settings channel <#channel>` to set a channel.\n'
+                        'Use `/settings channel` to unset the reminder channel.\n' 
+            },
+        ]
+        embed['color'] = constants.BLURPLE
+
+        await ctx.respond(embed=discord.Embed.from_dict(embed))
+    
+    settings_group = discord.commands.SlashCommandGroup("settings", "Change the settings for this server")
+
+    @settings_group.command()
+    @discord.option('offset', type=int, min_value=-12, max_value=12, required=True,
+        description='The number of hours to offset by')
+    async def offset(self, ctx: discord.ApplicationContext, offset: int):
+        """Set the UTC offset for this server"""
+        data.set_offset(ctx.guild_id, offset)
+        embed = {
+            'color': constants.BLURPLE,
+            'title': 'Setting changed!',
+            'description': f"New UTC Offset: {'+' if offset >= 0 else ''}{offset}"
+        }
+        await ctx.respond(embed=discord.Embed.from_dict(embed))
+    
+    @settings_group.command()
+    @discord.option('channel', type=str, required=False,
+        description='The channel to send reminders to')
+    async def channel(self, ctx: discord.ApplicationContext, channel):
+        """Set the reminder channel for this server"""
+        if channel:
+            # Channel must be in this guild 
+            channel_id = channel.strip(' <#>')
+            channel = self.bot.get_channel(channel_id)
+            if channel is None or channel.guild != ctx.guild:
+                ctx.respond(f'No channel <#{channel_id}> found in this server.', ephemeral=True)
+
+
+            data.set_target(ctx.guild_id, channel_id)
+            description = f'New Reminder Channel: <#{channel_id}>'
+        else:
+            data.set_target(ctx.guild_id, None)
+            description = 'Reminder channel unset.'
+        
+        embed = {
+            'color': constants.BLURPLE,
+            'title': 'Setting changed!',
+            'description': description
+        }
+        await ctx.respond(embed=discord.Embed.from_dict(embed))    
