f04decb24e5b9446d94fd9ed52a66e3073d867d8
Franco Reyes Mac <franco.javier.reyes@gmail.com>
2022-05-17 16:19:00 +1000

    Added regex for parsing intervals

--- src/constants.py
+++ src/constants.py
@@ -1,13 +1,35 @@
+import re
 from datetime import timedelta
+
 from dateutil.relativedelta import relativedelta
 
-DELTA = {
-    'minutes': timedelta(minutes=1),
-    'hours': timedelta(hours=1),
-    'days': timedelta(days=1),
-    'weeks': timedelta(days=7),
-    'months': relativedelta(months=1)
-}
+
+# REGEX taken from
+# https://github.com/wroberts/pytimeparse/blob/master/pytimeparse/timeparse.py
+
+YEARS       = r'(?P<year>\d+)\s*(?:ys?|yrs?.?|years?)'
+MONTHS      = r'(?P<month>\d+)\s*(?:mos?.?|mths?.?|months?)'
+WEEKS       = r'(?P<week>[\d.]+)\s*(?:w|wks?|weeks?)'
+DAYS        = r'(?P<day>[\d.]+)\s*(?:d|dys?|days?)'
+HOURS       = r'(?P<hour>[\d.]+)\s*(?:h|hrs?|hours?)'
+MINS        = r'(?P<minute>[\d.]+)\s*(?:m|(mins?)|(minutes?))'
+SECS        = r'(?P<second>[\d.]+)\s*(?:s|secs?|seconds?)'
+SEPARATORS  = r'[,/]'
+
+OPT         = lambda x: r'(?:{x})?'.format(x=x)
+OPTSEP      = lambda x: r'(?:{x}\s*(?:{SEPARATORS}\s*)?)?'.format(
+                            x=x, SEPARATORS=SEPARATORS)
+
+INTERVAL_FORMAT = r'{YEARS}\s*{MONTHS}\s*{WEEKS}\s*{DAYS}\s*{HOURS}\s*{MINS}\s*{SECS}'.format(
+    YEARS=OPTSEP(YEARS),
+    MONTHS=OPTSEP(MONTHS),
+    WEEKS=OPTSEP(WEEKS),
+    DAYS=OPTSEP(DAYS),
+    HOURS=OPTSEP(HOURS),
+    MINS=OPTSEP(MINS),
+    SECS=OPT(SECS))
+
+INTERVAL_REGEX = re.compile(r'\s*' + INTERVAL_FORMAT + r'\s*$', re.I)
 
 BLURPLE = 0x5865f2
 GREEN = 0x57f287
