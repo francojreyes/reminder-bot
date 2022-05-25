fae4e3646a4bb03665c23bb9ff1a0db2271bfc21
Franco Reyes Mac <franco.javier.reyes@gmail.com>
2022-05-25 16:19:26 +1000

    Flattened timezone list

--- src/constants.py
+++ src/constants.py
@@ -44,4 +44,5 @@ RED = 0xed4245
 
 with open('tzdata/tz_countries.json', 'r') as f:
     TZ_COUNTRIES = json.load(f)
-    TZ_ALL = list(itertools.chain(tz_list for tz_list in TZ_COUNTRIES.values()))
+    TZ_ALL = list(itertools.chain(*[tz_list for tz_list in TZ_COUNTRIES.values()]))
+    print(TZ_ALL)
